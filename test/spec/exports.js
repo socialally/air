1a42f988af002fc0cdb2c63cd2ad5b4586e5bd22
cyberfunk <hello@cyberfunk.org>
2015-02-26 11:44:42 +0800

    Update mocha setup.

--- test/spec/exports.js
+++ test/spec/exports.js
@@ -2,8 +2,9 @@ var $ = require('air')
   , expect = require('chai').expect;
 
 describe('Core:', function() {
-  it('should export function(s)', function() {
+  it('should export function(s)', function(done) {
     expect($).to.be.a('function');
     expect($.plugin).to.be.a('function');
+    done();
   });
 });
