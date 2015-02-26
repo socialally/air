acff13e0db7f5a1e416fd0e6f3de05bb84519b92
cyberfunk <hello@cyberfunk.org>
2015-02-26 12:02:17 +0800

    Test global leak detection.
    Currently, mocha appears to be working as expected in the browser but
    when running under phantomjs the mock global leak variable is not
    detected, requires more investigation.

--- test/spec/exports.js
+++ test/spec/exports.js
@@ -3,8 +3,11 @@ var $ = require('air')
 
 describe('Core:', function() {
   it('should export function(s)', function(done) {
+    //window.a = 2;
+    //window.variable = 'mew';
     expect($).to.be.a('function');
     expect($.plugin).to.be.a('function');
+    //console.log(a);
     done();
   });
 });
