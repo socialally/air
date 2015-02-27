4b1955a3dd111e5872d286358d1fd012c923fa1e
cyberfunk <hello@cyberfunk.org>
2015-02-27 10:26:36 +0800

    Assert on wrapped element (create plugin).

--- test/spec/create.js
+++ test/spec/create.js
@@ -11,4 +11,19 @@ describe('Create:', function() {
     done();
   });
 
+  it('should create DOM element', function(done) {
+    var tag = 'div'
+      , div = $.create(tag);
+    expect(div.tagName.toLowerCase()).to.eql(tag)
+    done();
+  });
+
+  it('should create wrapped element', function(done) {
+    var tag = 'div'
+      , div = $.el(tag);
+    expect(div).to.be.instanceof($.Air);
+    expect(div.get(0).tagName.toLowerCase()).to.eql(tag)
+    done();
+  });
+
 });
