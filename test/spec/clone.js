7fb40c81377f7e5d7f221ba14c7fb4c28262bca7
cyberfunk <hello@cyberfunk.org>
2015-03-01 08:11:39 +0800

    Assert on clone plugin.

--- test/spec/clone.js
+++ test/spec/clone.js
@@ -14,7 +14,15 @@ describe('Clone:', function() {
   afterEach(util.after);
 
   it('should deep clone elements', function(done) {
-    var el = $('.clone');
+    var el = $('.clone')
+      , cloned = el.clone()
+      , span = cloned.dom[0].firstChild
+      , strong = span.firstChild;
+    expect(el === cloned).to.eql(false);
+    expect(cloned.length).to.eql(el.length);
+    expect(span.tagName.toLowerCase()).to.eql('span');
+    expect(span.getAttribute('title')).to.eql('foo');
+    expect(strong.tagName.toLowerCase()).to.eql('strong');
     done();
   });
 
