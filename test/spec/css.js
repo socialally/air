9f7df52694772143edc1a14e0054b5bd057724ee
cyberfunk <hello@cyberfunk.org>
2015-03-01 08:30:22 +0800

    Update css test spec.

--- test/spec/css.js
+++ test/spec/css.js
@@ -13,8 +13,32 @@ describe('Css:', function() {
   })
   afterEach(util.after);
 
-  it('should get computed style', function(done) {
+  it('should return self on no elements', function(done) {
+    var el = $();
+    expect(el.css()).to.equal(el);
+    done();
+  });
+
+  it('should return null for non-existent style property', function(done) {
     var el = $('.css');
+    expect(el.css('non-existent-style')).to.eql(null);
+    done();
+  });
+
+  it('should get all styles', function(done) {
+    var el = $('.css')
+      , styles = el.css();
+
+    expect(styles).to.be.an('object');
+    done();
+  });
+
+  it('should get computed style', function(done) {
+    var el = $('.css')
+      , color = el.css('color')
+      , background = el.css('background-color');
+    expect(color).to.be.a('string');
+    expect(background).to.be.a('string');
     done();
   });
 
