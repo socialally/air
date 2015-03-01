479d34c8b5d0674255b3805b91265fbc24974387
cyberfunk <hello@cyberfunk.org>
2015-03-01 09:08:21 +0800

    Update append plugin.
    Do not clone on last matched element.

--- test/spec/append.js
+++ test/spec/append.js
@@ -28,17 +28,38 @@ describe('Append:', function() {
     done();
   });
 
-  it('should append multiple elements', function(done) {
+  it('should move element on single target element', function(done) {
     // fixture container
     var fx = $(util.selector);
     expect(fx.get(0).childNodes.length).to.eql(0);
     // append the mock element(s)
-    var ap = $('.append');
+    var ap = $('.append-multi');
+
+    // only matched a single target so the target is not cloned
+    // multiple calls result in it being *moved* to the same parent
     fx.append(ap)
       .append(ap)
       .append(ap);
-    // fixture should now have multiple child elements appended
-    expect(fx.get(0).childNodes.length).to.eql(3);
+    console.log(fx.get(0).innerHTML)
+    // fixture should now have single child element
+    expect(fx.get(0).childNodes.length).to.eql(1);
+    done();
+  });
+
+  it('should clone element on multiple target elements', function(done) {
+    var fx = $('.append-parent')
+      , ap = $('.append-child')
+      , node = ap.get(0);
+    fx.append(ap);
+    $('.append-child').each(function(el, index) {
+      if(!index) {
+        // this element should have been cloned
+        expect(el === node).to.eql(false);
+      }else{
+        // last match moved the content element
+        expect(el === node).to.eql(true);
+      }
+    })
     done();
   });
 
